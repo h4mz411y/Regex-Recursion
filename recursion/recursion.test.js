e8dcde6cfdcb60a20e3213c1b2ff7535590eb0ec
h4mz411y <hamzabashar93@yahoo.com>
2022-05-03 07:47:47 +0300

    recommited

--- recursion/recursion.test.js
+++ recursion/recursion.test.js
@@ -25,11 +25,8 @@ function pow(x,n){
 
 /* The Fibonacci Series is a numeric series starting with the integers 0 and 1. In this series,
 the next integer is determined by summing the previous two. This gives us:
-
 0, 1, 1, 2, 3, 5, 8, 13, ...
-
 Write a function that take n as parameter and return the nth element in the Fibonacci Series
-
 Example: n = 4 ==> 3, n= 0 ==> 0, n = 3 ==> 2 */
 
 function fibonacci(n){
@@ -38,11 +35,8 @@ function fibonacci(n){
 }
 
 /* Optional 
-
 The set [1, 2, 3, ..., n] contains a total of n! unique permutations.
-
 By listing and labeling all of the permutations in order, we get the following sequence for n = 3:
-
 "123"
 "132"
 "213"
@@ -50,7 +44,6 @@ By listing and labeling all of the permutations in order, we get the following s
 "312"
 "321"
 Given n and k, return the kth permutation sequence.
-
 Example: 
 Input: n = 3, k = 3
 Output: ["123", "132", "213", "231", "312", "321"] */
